3da79e031a4cfd74587f31833d587bdfabfc527b
holanliao <holanliao@hwsh.ylc.edu.tw>
2025-08-29 11:44:59 +0800

    Update 3-3-01.c

--- C_CodeExample/3-3-01.c
+++ C_CodeExample/3-3-01.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main(void){
-	char a = 'h';
-	char b[] = "Holan";
-	int c = 20240928;
-	long d = 97;
-	float e = 2.71828182845904523536028747135;
-	double f = 2.71828182845904523536028747135;
+	char ch = 'h';
+	char str[] = "Holan";
+	int i = 20240928;
+	long longInt = 97;
+	float singleFloat = 2.71828182845904523536028747135;
+	double doubleFloat = 2.71828182845904523536028747135;
 
-	printf("¦L¥X¦r¤¸ %c\n", a);
-	printf("¦L¥X¦r¦ê %s\n", b);
-	printf("¦L¥X¾ã¼Æ %d\n", c);
-	printf("¦L¥Xªø¾ã¼Æ %ld\n", d);
-	printf("¦L¥X³æºë«×¯BÂI¼Æ %f\n", e);
-	printf("¦L¥X­¿ºë«×¯BÂI¼Æ %lf\n", f);
-	printf("¦L¥X %%");
+	printf("å­—å…ƒ %c\n", ch);
+	printf("å­—ä¸² %s\n", str);
+	printf("æ•´æ•¸ %d\n", i);
+	printf("é•·æ•´æ•¸ %ld\n", longInt);
+	printf("å–®ç²¾åº¦æµ®é»æ•¸ %f\n", singleFloat);
+	printf("å€ç²¾åº¦æµ®é»æ•¸ %lf\n", doubleFloat);
+	printf("è¼¸å‡º %%");
 
 	return 0;
 }
+
